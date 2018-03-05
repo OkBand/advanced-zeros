0f2db0f084ce736a463be2634906a36d108f8512
OkBand <o.bandura42@gmail.com>
2018-03-06 00:01:15 +0300

    try

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 module.exports = function getZerosCount(number, base) {
-  for (var i = 2; i <= base; i++) {   //finding prime factors
-    if (base % i === 0) {
-         base = base/i;
-      var a = 0;
-      var b = Math.floor(number/i);
-      while (b > 0) {
-        a = a + b;
-        b = Math.floor(b/i);
-      };
-      var zeros = a;
+  var primes = [];
+  for (var i = 2; i <= base; i++) {
+    while (base % i === 0) {
+      primes[i] = primes[i] + 1;
+      base = base / i;
     };
+  };
+
+  var zeros = 0;
+  for (var i = 2; i < primes.length; i++) {
+    var a = 0;
+    var b = Math.floor(number / i);
+    while (b > 0) {
+      a = a + b;
+      b = Math.floor(b / i);
+    };
+    var zeros = a;
   };
   return zeros;
 }
